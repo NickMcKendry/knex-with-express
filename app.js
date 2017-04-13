0e2560823eb933325527fff2296de39c553d65ae
Nick McKendry <nickmckendry@gmail.com>
2017-04-13 10:15:39 -0600

    sending stuff

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express")
 
 const app = express()
-const port = 3000
+const port = 3001
+
+app.get("/api/planets", (req, res) => {
+  res.send('Space Yo')
+})
 
 app.listen(port, () => {
   console.log(`api listening on part ${port}`);
