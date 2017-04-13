14e48b2d59031b6f22768da775683dc5f20b7a20
Nick McKendry <nickmckendry@gmail.com>
2017-04-13 11:04:58 -0600

    test

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express")
+const pg = require('./db/knex')
 
 const app = express()
-const port = 3001
+const port = 3002
 
 app.get("/api/planets", (req, res) => {
-  res.send('Space Yo')
+  pg('planet').select().then((data) => {
+    res.json(data)
+    //if you had a front end this would rneder through hbs
+    // res.render('/', {data})
+  })
 })
 
 app.listen(port, () => {
--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,7 @@
 const config = require('../knexfile')
 
-const enviroment = process.env.DATABASE_URL || 'development'
+const enviroment = process.env.NODE_ENV || 'development'
 
 const pg = require('knex')(config[enviroment])
+
+module.exports = pg
